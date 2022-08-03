dd8257c656b10debffd6f8fb82898b1776a81a3f
Aristiklever R. Sousa <aristikleverrodrgues@gmail.com>
2022-08-03 19:55:13 -0300

    :wrench: chore: Add api file for the axios configuring.

---
+++ src/common/api.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const Client = axios.create({
+  baseURL: "https://dogbreed-api.q9.com.br/",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+});
+
+export default Client;
