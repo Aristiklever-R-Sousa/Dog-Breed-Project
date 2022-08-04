cbf6afc71339e44d4f1a0f3c393f6755b845fe6c
Aristiklever R. Sousa <aristikleverrodrgues@gmail.com>
2022-08-04 11:39:31 -0300

    :sparkles: feat: Additing in the form the communication with public API.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,33 +1,74 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import Client from "../../common/api";
+import { CurrentUserContext } from "../../context/CurrentUserContext";
+import { UserType } from "../../types/User";
 
-// interface ILogin {}
 import "./login.scss";
 
 const Login: React.FC = () => {
-  const handleRequisition = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const userContext = useContext(CurrentUserContext);
+  const [email, setEmail] = useState<string>("");
+  const [isSubmiting, setIsSubmiting] = useState<boolean>(false);
+
+  const handleError = (mess: string) => {
+    alert(mess);
+    setIsSubmiting(false);
+  };
+
+  const handleRequisition = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Requisitando: ");
+    setIsSubmiting(true);
+
+    Client.post("/register", null, {
+      headers: {
+        email,
+      },
+    })
+      .then((res: { data: { user: UserType } }) => {
+        const { user } = res.data;
+        user.email = email;
+
+        console.log(user, "REQUISITION");
+
+        if (user) {
+          if (userContext) userContext.setCurrentUser(user);
+          // STORING THE TOKEN RETURNED FOR THE ENDPOINT
+          localStorage.setItem("dog_breed_token", user.token);
+        } else handleError("An error ocurred");
+        setIsSubmiting(false);
+      })
+      .catch((err) => {
+        handleError("An erro ocurred (log in console).");
+        console.log(err);
+        setIsSubmiting(false);
+      });
   };
 
   return (
     <div className="container">
-      <form method="POST">
+      <form method="POST" onSubmit={handleRequisition}>
         <fieldset>
           <legend>Formulário aqui</legend>
           <div className="form-content">
             <div className="box-element">
               <label htmlFor="email">Email</label>
               <br />
-              <input type="email" id="email" className="email" />
+              <input
+                type="email"
+                id="email"
+                className="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="box-element">
-              <button type="submit" value="Entrar" onClick={handleRequisition}>
+              <button type="submit" value="Entrar">
                 Entrar
               </button>
             </div>
           </div>
         </fieldset>
       </form>
+      {isSubmiting && <span>Está submetendo</span>}
     </div>
   );
 };
